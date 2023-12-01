33e6cda9004e33f9f28a7d3447132532d7662c42
David Whitney <david@davidwhitney.co.uk>
2023-12-01 06:16:02 +0000

    wip

--- src/day1.test.ts
+++ src/day1.test.ts
@@ -1,10 +1,65 @@
 import day1 from "./day1";
 import { describe, it, beforeEach, expect } from "vitest";
+import * as fs from "fs";
 
 describe('day1', () => {
 
-    it('something', () => {
-        expect(1).toBe(1);
+    // it('part 1 example', () => {
+    //     var input = [
+    //         "1abc2",
+    //         "pqr3stu8vwx",
+    //         "a1b2c3d4e5f",
+    //         "treb7uchet"
+    //     ];
+
+    //     const result = day1(input);
+
+    //     expect(result).toBe(142)
+    // });
+
+    // it('part 1 solution', () => {
+    //     const text = fs.readFileSync('./src/day1.input.txt', 'utf-8');
+    //     const input = text.split('\n');            
+
+    //     const result = day1(input);
+
+    //     expect(result).toBe(54388);
+    // });
+
+    // it('part 2 test', () => {
+    //     var input = [
+    //         "two1nine",
+    //         "eightwothree",
+    //         "abcone2threexyz",
+    //         "xtwone3four",
+    //         "4nineeightseven2",
+    //         "zoneight234",
+    //         "7pqrstsixteen"
+    //     ];           
+
+    //     const result = day1(input, true);
+
+    //     expect(result).toBe(281);
+    // });
+
+    it('part 2 edge cases', () => {
+        var input = [
+            "eighthree",
+        ];           
+
+        const result = day1(input, true);
+
+        expect(result).toBe(83);
     });
 
+    // it('part 2 solution', () => {
+    //     const text = fs.readFileSync('./src/day1.input.txt', 'utf-8');
+    //     const input = text.split('\n');            
+
+    //     const result = day1(input, true);
+
+    //     expect(result).toBe(-1);
+    // });
+
 });
+
--- src/day1.ts
+++ src/day1.ts
@@ -1,3 +1,46 @@
-export default function day1() {
-    return 1;
+export default function day1(input: string[], supportWords = false) {
+
+    const theWordsOneToNine = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+
+    if (supportWords) {
+        const newInput = [];
+
+        for(let line of input) {
+            let cursor = 0;
+
+
+            while (cursor <= line.length) {
+                const slice = line.slice(0, cursor); //?
+                
+                for(let word of theWordsOneToNine) {
+                    if (slice.includes(word)) {
+                        const number = theWordsOneToNine.indexOf(word) + 1 + '';
+                        line = line.replace(word, number);
+                        cursor = -1;
+                    }
+                }
+
+                if (cursor === -1) {
+                    cursor = 0;
+                } else {
+                    cursor++;
+                }
+            }
+
+            newInput.push(line);        
+        }
+
+        input = newInput; //?
+    }
+
+    const regex = /\D/g;
+    const withoutChars = input.map(x => x.replace(regex, ''));
+    console.log(withoutChars);
+
+    const toNumbers = withoutChars.map(x => {
+        const firstAndLast = x.slice(0, 1) + x.slice(-1);
+        return parseInt(firstAndLast);
+    });
+    const sum = toNumbers.reduce((a, b) => a + b, 0);
+    return sum;
 }
