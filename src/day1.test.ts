bd52d97b47f0d5f50364eced1fa7a60cfa0986f7
David Whitney <david@davidwhitney.co.uk>
2023-12-01 04:25:47 +0000

    prepare

---
+++ src/day1.test.ts
@@ -0,0 +1,10 @@
+import day1 from "./day1";
+import { describe, it, beforeEach, expect } from "vitest";
+
+describe('day1', () => {
+
+    it('something', () => {
+        expect(1).toBe(1);
+    });
+
+});
