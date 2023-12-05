b1030a50f4d221529532ecd078237d14638499d9
David Whitney <david@davidwhitney.co.uk>
2023-12-05 00:33:57 +0000

    F# impl.

---
+++ day4-fsharp/Day4/Solution.fs
@@ -0,0 +1,57 @@
+﻿module Day4
+
+type Card = {
+    Name: string
+    Number: int
+    WinningNumbers: int[]
+    PlayerNumbers: int[]
+    Points: int
+    NumberOfWins: int
+    ExtraCardIds: int list
+}
+
+let toNumbers (parts: string) = parts.Split(' ') |> Array.filter (fun x -> x.Trim() <> "") |> Array.map int
+
+let totalCards (cards: Card array) =
+    let cardsInstancesCount = cards |> Array.map (fun _ -> 1)
+    let mutable totalSum = 0
+
+    for card in cards do
+        let cardId = card.Number - 1
+        totalSum <- totalSum + cardsInstancesCount.[cardId]
+
+        for index in cardId + 1 .. min (cardId + card.NumberOfWins) (cards.Length - 1) do
+            cardsInstancesCount.[index] <- cardsInstancesCount.[index] + cardsInstancesCount.[cardId]
+
+    totalSum
+
+let solve (text: string) =
+    let cards = 
+        text.Split([|'\r'; '\n'|], System.StringSplitOptions.RemoveEmptyEntries)
+        |> Array.map (fun card ->
+            let parts = card.Split(':')
+            let name = parts.[0].Trim()
+            let numberParts = parts.[1].Trim().Split('|')
+            let number = int(name.Replace("Card", "").Trim())
+            let winningNumbers = toNumbers(numberParts.[0])
+            let playerNumbers = toNumbers(numberParts.[1])
+
+            let numberOfWins = playerNumbers |> Array.filter (fun x -> Array.contains x winningNumbers) |> Array.length
+            let points = int(2.0 ** float(numberOfWins - 1))
+
+            let extraCardIds = [for i in 1 .. numberOfWins -> number + i]
+
+            { 
+                Name = name; 
+                Number = number; 
+                WinningNumbers = winningNumbers; 
+                PlayerNumbers = playerNumbers; 
+                Points = points; 
+                NumberOfWins = numberOfWins; 
+                ExtraCardIds = extraCardIds 
+            }
+        )
+
+    let totalCards = totalCards(cards)
+    let sumOfPoints = cards |> Array.sumBy (fun card -> card.Points)    
+    (sumOfPoints, totalCards)
---
+++ day4-fsharp/Day4/Tests.fs
@@ -0,0 +1,32 @@
+﻿module Tests
+
+open Day4
+
+open NUnit.Framework
+open FsUnit
+
+[<TestFixture>]
+type Day4Tests() =
+
+    [<Test>]
+    member this.``Example Test`` () =
+        let input = 
+            """
+            Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+            Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+            Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+            Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+            Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+            Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+            """.Trim()
+        
+        let sumOfPoints, totalCards = solve input
+        sumOfPoints |> should equal 13
+        totalCards |> should equal 30
+
+    [<Test>]
+    member this.``Solution Test`` () =
+        let input = System.IO.File.ReadAllText("./input.txt")
+        let sumOfPoints, totalCards = solve input
+        sumOfPoints |> should equal 28538
+        totalCards |> should equal 9425061
